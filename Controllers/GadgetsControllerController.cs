28a4092b853d4a0f8370abd7280d158e01e398bb
Olexander Tsyganok <Ciga_fu80@student.itstep.org>
2023-01-09 19:32:01 +0200

    add controller

---
+++ Controllers/GadgetsControllerController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleConnectio.DBController;
+using SimpleConnectio.Models;
+
+namespace SimpleConnectio.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GadgetsControllerController : Controller
+    {
+
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IResult Get()
+        {
+            return Results.Json(new SmartphonesController().GetAll());
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IResult Add([FromForm]Smartphone smartphone)
+        {
+            return new SmartphonesController().Add(smartphone) ? Results.StatusCode(201) : Results.StatusCode(400);
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public IResult Remove([FromForm] int id)
+        {
+            return new SmartphonesController().Remove(id) ? Results.StatusCode(200) : Results.StatusCode(400);
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public IResult Update([FromForm] int id, Smartphone smartphone)
+        {
+            return new SmartphonesController().Update(id, smartphone) ? Results.StatusCode(200) : Results.StatusCode(400);
+        }
+    }
+}
---
+++ DBController/SmartphonesController.cs
@@ -0,0 +1,59 @@
+﻿using SimpleConnectio.Models;
+
+namespace SimpleConnectio.DBController
+{
+    public class SmartphonesController
+    {
+
+        public IEnumerable<Smartphone> GetAll()
+        {
+            return new SmartphonesDataBaseContext().Smartphones.ToList();
+        }
+
+        public bool Add(Smartphone smartphone)
+        {
+            if (smartphone == null)
+                return false;
+
+            using (SmartphonesDataBaseContext context = new SmartphonesDataBaseContext())
+            {
+                context.Smartphones.Add(smartphone);
+                context.SaveChanges();
+                return true;
+            }
+        }
+
+        public bool Remove(int id)
+        {
+            if (id <= 0)
+                return false;
+
+            using (SmartphonesDataBaseContext context = new SmartphonesDataBaseContext())
+            {
+                var con = context.Smartphones.FirstOrDefault(x => x.Id == id);
+                if (con == null)
+                    return false;
+                context.Remove(con);
+                context.SaveChanges();
+                return true;
+            }
+        }
+
+        public bool Update(int id, Smartphone smartphone)
+        {
+            if (id <= 0)
+                return false;
+
+            using (SmartphonesDataBaseContext context = new SmartphonesDataBaseContext())
+            {
+                var con = context.Smartphones.FirstOrDefault(x => x.Id == id);
+                if (con == null)
+                    return false;
+                con.Model = con.Model;
+                con.Name = con.Name;
+                context.SaveChanges();
+                return true;
+            }
+        }
+    }
+}
